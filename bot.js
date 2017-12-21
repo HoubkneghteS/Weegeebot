be40c3079e7a3b522303e1a58c770c09c244ace5
Adam Simons <houbkneghteS@users.noreply.github.com>
2017-12-20 23:43:19 -0600

    tweaks

--- bot.js
+++ bot.js
@@ -471,7 +471,7 @@ function vapor(text) {
 	let startText = [/a/gi, /b/gi, /c/gi, /d/gi, /e/gi, /f/gi, /g/gi, /h/gi, /i/gi, /j/gi, /k/gi, /l/gi, /m/gi, /n/gi, /o/gi, /p/gi, /q/gi, /r/gi, /s/gi, /t/gi, /u/gi, /v/gi, /w/gi, /x/gi, /y/gi, /z/gi, /1/g, /2/g, /3/g, /4/g, /5/g, /6/g, /7/g, /8/g, /9/g, /0/g], //starting letters
 	endText = ["ａ", "ｂ", "ｃ", "ｄ", "ｅ", "ｆ", "ｇ", "ｈ", "ｉ", "ｊ", "ｋ", "ｌ", "ｍ", "ｎ", "ｏ", "ｐ", "ｑ", "ｒ", "ｓ", "ｔ", "ｕ", "ｖ", "ｗ", "ｘ", "ｙ", "ｚ", "１", "２", "３", "４", "５", "６", "７", "８", "９", "０"]; //resulting letters
 
-	for (let i = 0; i < startText.length; i++) {
+	for (var i = 0; i < startText.length; i++) {
 		text = text.replace(startText[i], endText[i]); //replaces text
 	}
 	return text;
@@ -482,7 +482,7 @@ function fraktur(text) {
 	let startText = [/ä/g, /ö/g, /ü/g, /ß/g, /Ä/g, /Ö/g, /Ü/g, /ẞ/g, /a/g, /b/g, /c/g, /d/g, /e/g, /f/g, /g/g, /h/g, /i/g, /j/g, /k/g, /l/g, /m/g, /n/g, /o/g, /p/g, /q/g, /r/g, /s/g, /t/g, /u/g, /v/g, /w/g, /x/g, /y/g, /z/g, /A/g, /B/g, /C/g, /D/g, /E/g, /F/g, /G/g, /H/g, /I/g, /J/g, /K/g, /L/g, /M/g, /N/g, /O/g, /P/g, /Q/g, /R/g, /S/g, /T/g, /U/g, /V/g, /W/g, /X/g, /Y/g, /Z/g], //starting letters
 	endText = ["ae", "oe", "ue", "ss", "Ae", "Oe", "Ue", "SS", "𝔞", "𝔟", "𝔠", "𝔡", "𝔢", "𝔣", "𝔤", "𝔥", "𝔦", "𝔧", "𝔨", "𝔩", "𝔪", "𝔫", "𝔬", "𝔭", "𝔮", "𝔯", "𝔰", "𝔱", "𝔲", "𝔳", "𝔴", "𝔵", "𝔶", "𝔷", "𝔄", "𝔅", "ℭ", "𝔇", "𝔈", "𝔉", "𝔊", "ℌ", "ℑ", "𝔍", "𝔎", "𝔏", "𝔐", "𝔑", "𝔒", "𝔓", "𝔔", "ℜ", "𝔖", "𝔗", "𝔘", "𝔙", "𝔚", "𝔛", "𝔜", "ℨ"]; //resulting letters
 
-	for (let i = 0; i < startText.length; i++) {
+	for (var i = 0; i < startText.length; i++) {
 		text = text.replace(startText[i], endText[i]); //replaces text
 	}
 	return text;
@@ -1119,7 +1119,7 @@ bot.on('message', msg => {
 			if (role(msg, "MANAGE_MESSAGES")) {
 				if (arg[1] <= 51) {
 					var m = add(2, msg);
-					let i;
+					var i;
 					for (i = 0; i < arg[1]; i++) {
 						msg.channel.send(m);
 					}
