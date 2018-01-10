43ef4f2354a9c75c61852f989ee7fdb6a0f9f209
Adam Simons <houbkneghteS@users.noreply.github.com>
2018-01-10 00:11:28 -0600

    tweaks

--- bot.js
+++ bot.js
@@ -863,11 +863,10 @@ bot.on('message', msg => {
 			else if (arg[1] <= 0 || arg[2] <= 0) msg.channel.send(r.battleError1); //if any army lacks soldiers
 			else {
 				var str1 = parseInt(arg[1]) ** 2, //strength of army 1
-					str2 = parseInt(arg[2]) ** 2, //strength of army 2
-					victory = (Math.floor(Math.random() * (str1 + str2)) <= parseInt(str1)) ? r.victory1 : r.victory2;
+					str2 = parseInt(arg[2]) ** 2; //strength of army 2
 
 				if (Math.floor(Math.random() * (str1 + str2)) <= parseInt(str1)) msg.channel.send(r.victory1);
-				else msg.channel.send();
+				else msg.channel.send(r.victory2);
 				cmdLog("battle", msg);
 			}
 			break;
